92872521f34562ab6fb968cb857055dc2c07da6e
PaulaRudy <PaulaRudy@users.noreply.github.com>
2016-08-15 17:59:11 -0400

    Added class for new connections via filters.

---
+++ src/cnnetwork/Filter.java
@@ -0,0 +1,34 @@
+package cnnetwork;
+
+import java.util.LinkedList;
+
+public class Filter {
+	double[][][] weights;// The actual stored weights of this filter
+	String equationAtFilter;// The equation used to calculate an output for this
+							// filter, stored in string form to be used with
+							// JavaCalculus
+	LinkedList<FilterConnection> connections;// A list of connections that use
+												// this filter, to be used in
+												// backpropagation.
+
+	public Filter(double[][][] weights, String equationAtFilter, LinkedList<FilterConnection> connections) {
+		this.weights = weights;
+		this.equationAtFilter = equationAtFilter;
+		this.connections = connections;
+	}
+
+	/**
+	 * Default constructor for a filter with values.
+	 * Initializes the equation to an empty string and sets up 
+	 * the linked list.
+	 * @param weights 
+	 * 			A 3-dimensional array of doubles that are the actual
+	 * 			stored weights of this filter
+	 */
+	public Filter(double[][][] weights) {
+		this.weights = weights;
+		this.equationAtFilter = "";
+		this.connections = new LinkedList<FilterConnection>();
+	}
+
+}

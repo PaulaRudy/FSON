6de951bb9b5431ff11945f6642093dbc533f5f85
PaulaRudy <PaulaRudy@users.noreply.github.com>
2016-09-05 17:07:43 -0400

    Added enum for softmax layers

--- src/cnnetwork/LayerType.java
+++ src/cnnetwork/LayerType.java
@@ -4,12 +4,13 @@ package cnnetwork;
  * An enum used to indicate the type of a Layer:
  * Convolutional (CONV),
  * Maxpooling (MAXPOOL),
- * Locally connected (LOCAL),
- * or 
- * Fully connected (FULLY).
+ * Locally connected (LOCAL), 
+ * Fully connected (FULLY),
+ * or
+ * Softmax (SOFTMAX).
  */
 public enum LayerType {
-	CONV(1), MAXPOOL(2), LOCAL(3), FULLY(4);
+	CONV(1), MAXPOOL(2), LOCAL(3), FULLY(4), SOFTMAX(5);
 
 	private int value;
 
